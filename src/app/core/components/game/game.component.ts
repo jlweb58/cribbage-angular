2fa020e29de61ecf460939fa6f35673b8e3da3cb
John Webber <jlweb58@gmail.com>
2025-12-03 15:34:24 +0100

    Added material; implemented primitive start game page

--- src/app/core/components/game/game.component.ts
+++ src/app/core/components/game/game.component.ts
@@ -1,19 +1,32 @@
 import { Component } from '@angular/core';
 import {GameService} from '../../services/game.service';
 import {Game} from '../../models/game';
+import {MatCard} from '@angular/material/card';
+import {MatButton} from '@angular/material/button';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-game',
-  imports: [],
+  imports: [
+    MatCard,
+    MatButton,
+  ],
   templateUrl: './game.component.html',
   styleUrl: './game.component.css',
 })
 export class GameComponent {
   game: Game | undefined;
 
-  constructor(private gameService: GameService) { }
+  constructor(private gameService: GameService, private router: Router  ) { }
 
+  onSubmit() {
+    this.gameService.startGame().subscribe({
+      next: game => {
+        this.game = game;
+        this.router.navigate(['/deal']);
+      },
+      error: error => console.error(error)
+    });
 
-
-
+  }
 }
